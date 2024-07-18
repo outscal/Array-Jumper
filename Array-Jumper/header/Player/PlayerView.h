a87050de4475a89d12c9fde96af9140304c55b63
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-07-18 15:00:51 +0530

    Commit

--- Array-Jumper/header/Player/PlayerView.h
+++ Array-Jumper/header/Player/PlayerView.h
@@ -1,31 +1,30 @@
 #pragma once
+#include "../../header/Level/LevelModel.h"
 #include "../../header/UI/UIElement/ImageView.h"
 
 
 namespace Player
 {
-	
 	class PlayerController;
 	class PlayerView
-		struct BoxDimensions;
-   
-
 	{
 	private:
+		// TODO: Fix spacing. References grouped together + properties grouped together. 
 		sf::RenderWindow* game_window;
-		UI::UIElement::ImageView* player_image;
 		PlayerController* player_controller;
-		BoxDimentions current_box_dimensions;
+		UI::UIElement::ImageView* player_image;
 
-		float player_height ;
+		float player_height;
 		float player_width;
+		Level::BoxDimensions current_box_dimensions;
 
 		void initializePlayerImage();
 		void drawPlayer();
 		void loadPlayer();
 		void CalculatePlayerDimensions();
+		void updatePlayerPosition();
 		sf::Vector2f calulcatePlayerPosition();
-		void CalculatePlayerDimensions();
+
 	public:
 		PlayerView(PlayerController* controller);
 		~PlayerView();
@@ -33,5 +32,6 @@ namespace Player
 		void initialize();
 		void update();
 		void render();
+
 	};
 }
--- Array-Jumper/source/Player/PlayerView.cpp
+++ Array-Jumper/source/Player/PlayerView.cpp
@@ -55,7 +55,9 @@ namespace Player
 	}
 	sf::Vector2f PlayerView::calulcatePlayerPosition()
 	{
-		return sf::Vector2f(0, 0);
+		float xPosition = current_box_dimensions.box_spacing + static_cast<float>(player_controller->getCurrentPosition()) * (current_box_dimensions.box_width + current_box_dimensions.box_spacing);
+		float yPosition = static_cast<float>(game_window->getSize().y) - current_box_dimensions.box_height - current_box_dimensions.bottom_offset - player_height;
+		return sf::Vector2f(xPosition, yPosition);
 	}
 	void PlayerView::initializePlayerImage()
 	{
