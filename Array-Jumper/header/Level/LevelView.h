85d1b9358733bdbbc7365cb9f5a2f465c333515f
Akashkapoor <150754201+akashkapoordev@users.noreply.github.com>
2024-11-01 18:30:22 -0400

    Draw a Box with Overlay

--- Array-Jumper/header/Level/LevelView.h
+++ Array-Jumper/header/Level/LevelView.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../../header/UI/UIElement/ImageView.h"
+#include "LevelModel.h"
 namespace Level
 {
 	class LevelController;
@@ -13,17 +14,30 @@ namespace Level
 		void update();
 		void render();
 
-	
-		
+		void calcuateBoxDimension();
+		UI::UIElement::ImageView* getBoxImage(BlockType type);
+
+		void drawBox(sf::Vector2f position);
+		void drawBoxValue(sf::Vector2f position, BlockType type);
 
 
 
 	private:
 		LevelController* level_controller;
 		UI::UIElement::ImageView* background_image;
+		UI::UIElement::ImageView* box_image;
 		sf::RenderWindow* game_window;
 
 		const float background_alpha = 110.f;
+
+		BoxDimension box_dimension;
+
+		UI::UIElement::ImageView* target_overlay_image;
+		UI::UIElement::ImageView* letter_one_overlay_image;
+		UI::UIElement::ImageView* letter_two_overlay_image;
+		UI::UIElement::ImageView* letter_three_overlay_image;
+		UI::UIElement::ImageView* obstacle_one_overlay_image;
+		UI::UIElement::ImageView* obstacle_two_overlay_image;
 
 		void  createImage();
 		void initilizeImage();
--- Array-Jumper/source/Level/LevelView.cpp
+++ Array-Jumper/source/Level/LevelView.cpp
@@ -5,6 +5,7 @@
 namespace Level
 {
 	using namespace Global;
+	using namespace UI::UIElement;
 
 	Level::LevelView::LevelView(LevelController* controller)
 	{
@@ -29,25 +30,102 @@ namespace Level
 	{
 		drawImage();
 	}
+	void LevelView::calcuateBoxDimension()
+	{
+		if (!game_window) return;
+
+		box_dimension.box_width = 300.f;
+		box_dimension.box_height = 300.f;
+	}
+	ImageView* LevelView::getBoxImage(BlockType type)
+	{
+		switch (type)
+		{
+		case BlockType::OBSTACLE_ONE:
+			return obstacle_one_overlay_image;
+			break;
+		case BlockType::OBSTACLE_TWO:
+			return obstacle_two_overlay_image;
+			break;
+		case BlockType::TARGET:
+			return target_overlay_image;
+			break;
+		case BlockType::ONE:
+			return letter_one_overlay_image;
+			break;
+		case BlockType::TWO:
+			return letter_two_overlay_image;
+			break;
+		case BlockType::THREE:
+			return letter_three_overlay_image;
+			break;
+		}
+	}
+	void LevelView::drawBox(sf::Vector2f position)
+	{
+		box_image->setPosition(position);
+		box_image->render();
+	}
+	void LevelView::drawBoxValue(sf::Vector2f position, BlockType type)
+	{
+		ImageView* image = getBoxImage(type);
+		image->setPosition(position);
+		image->render();
+	}
 	void LevelView::createImage()
 	{
-		background_image = new UI::UIElement::ImageView();
+		background_image = new ImageView();
+		box_image = new ImageView();
+		target_overlay_image = new ImageView();
+		letter_one_overlay_image = new ImageView();
+		letter_two_overlay_image = new ImageView();
+		letter_three_overlay_image = new ImageView();
+		obstacle_one_overlay_image = new ImageView();
+		obstacle_two_overlay_image = new ImageView();
+
+
 	}
 	void LevelView::initilizeImage()
 	{
 		background_image->initialize(Config::array_jumper_bg_texture_path, game_window->getSize().x, game_window->getSize().y, sf::Vector2f(0, 0));
 		background_image->setImageAlpha(background_alpha);
+
+		box_image->initialize(Config::box_texture_path, box_dimension.box_width, box_dimension.box_height, sf::Vector2f(0, 0));
+		//target_overlay_image->initialize(Config::target_texture_path, box_dimension.box_width, box_dimension.box_height, sf::Vector2f(0, 0));
+		letter_one_overlay_image->initialize(Config::letter_one_texture_path, box_dimension.box_width, box_dimension.box_height, sf::Vector2f(0, 0));
+		letter_two_overlay_image->initialize(Config::letter_two_texture_path, box_dimension.box_width, box_dimension.box_height, sf::Vector2f(0, 0));
+		letter_three_overlay_image->initialize(Config::letter_three_texture_path, box_dimension.box_width, box_dimension.box_height, sf::Vector2f(0, 0));
+		obstacle_one_overlay_image->initialize(Config::obstacle_01_texture_path, box_dimension.box_width, box_dimension.box_height, sf::Vector2f(0, 0));
+		obstacle_two_overlay_image->initialize(Config::obstacle_02_texture_path, box_dimension.box_width, box_dimension.box_height, sf::Vector2f(0, 0));
 	}
 	void LevelView::updateImage()
 	{
 		background_image->update();
+		box_image->update();
+		target_overlay_image->update();
+		letter_one_overlay_image->update();
+		letter_two_overlay_image->update();
+		letter_three_overlay_image->update();
+		obstacle_one_overlay_image->update();
+		obstacle_two_overlay_image->update();
 	}
 	void LevelView::drawImage()
 	{
 		background_image->render();
+		drawBox(sf::Vector2f(0, 0));
+		BlockType blockTypeToDraw = level_controller->getCurrentBoxValue(0);
+		drawBoxValue(sf::Vector2f(0, 0), blockTypeToDraw);
+	
 	}
 	void LevelView::deleteImage()
 	{
 		delete(background_image);
+		delete(box_image);
+		delete(target_overlay_image);
+		delete(letter_one_overlay_image);
+		delete(letter_two_overlay_image);
+		delete(letter_three_overlay_image);
+		delete(obstacle_one_overlay_image);
+		delete(obstacle_two_overlay_image);
 	}
 }
