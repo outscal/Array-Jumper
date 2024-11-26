b0b9c3ddfa0e4042ede5b9b388d08ef21a649565
meteoralpha23 <luckycreed123@gmail.com>
2024-11-26 06:20:00 +0530

    Creating Player Model

---
+++ Array-Jumper/header/Player/PlayerView.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "../../header/UI/UIElement/ImageView.h"
+namespace Player
+{
+
+	class PlayerView
+	{
+	private:
+		sf::RenderWindow* game_window;
+		UI::UIElement::ImageView* player_image;
+
+		float player_height;
+		float player_width;
+
+		void initializePlayerImage(); //Initialize player_image 
+		void drawPlayer(); //Called every frame to render the sprite on the screen
+		void loadPlayer(); //Loads the information needed to render the player
+		void calculatePlayerDimensions(); //Calculate the size of image
+		void updatePlayerPosition(); //Updates the Player Sprite's position on each frame
+		sf::Vector2f calulcatePlayerPosition();
+	public:
+		PlayerView();
+		~PlayerView();
+
+		void initialize();
+		void update();
+		void render();
+
+
+	};
+
+
+
+
+}
